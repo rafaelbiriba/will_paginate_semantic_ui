75b120d30e18cda432196bf497e038eff088f0dc
Rafael Biriba <biribarj@gmail.com>
2016-07-08 10:04:36 -0300

    Adding Rails support

--- lib/will_paginate_semantic_ui.rb
+++ lib/will_paginate_semantic_ui.rb
@@ -1,5 +1,6 @@
-module WillPaginateSemanticUi
-end
-
 require "will_paginate_semantic_ui/version"
-require "will_paginate_semantic_ui/generic_renderer"
+require "will_paginate"
+
+if defined?(ActionView)
+  require "will_paginate_semantic_ui/rails"
+end
---
+++ lib/will_paginate_semantic_ui/rails.rb
@@ -0,0 +1,8 @@
+require "will_paginate/view_helpers/action_view"
+require "will_paginate_semantic_ui/generic_renderer"
+
+module WillPaginateSemanticUi
+  class Renderer < WillPaginate::ActionView::LinkRenderer
+    include WillPaginateSemanticUi::GenericRenderer
+  end
+end
