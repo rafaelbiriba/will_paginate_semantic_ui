a2871cff03aae03467ea93be3d6c49e75ed54fe7
Rafael Biriba <biribarj@gmail.com>
2016-07-07 22:39:26 -0300

    Adding a Generic Renderer for Will Paginate

--- lib/will_paginate_semantic_ui.rb
+++ lib/will_paginate_semantic_ui.rb
@@ -2,3 +2,4 @@ module WillPaginateSemanticUi
 end
 
 require "will_paginate_semantic_ui/version"
+require "will_paginate_semantic_ui/generic_renderer"
---
+++ lib/will_paginate_semantic_ui/generic_renderer.rb
@@ -0,0 +1,45 @@
+module WillPaginateSemanticUi
+  module GenericRenderer
+    GAP = "&hellip;"
+
+    def to_html
+      list_items = pagination.map do |item|
+        item.class == Fixnum ? page_number(item) : send(item)
+      end.join(@options[:link_separator])
+
+      tag("div", list_items, class: ul_class)
+    end
+
+    protected
+
+    def page_number(page)
+      classLink = "item"
+      classLink += " active" if page == current_page
+      link(page, page, {class: classLink})
+    end
+
+    def previous_or_next_page(page, classname)
+      classLink = "icon item"
+      classLink += " disabled" if !page
+      link(tag("i", "", class: "#{classname} chevron icon"), page, {class: classLink})
+    end
+
+    def gap
+      tag("div", GAP, class: "disabled item")
+    end
+
+    def previous_page
+      num = @collection.current_page > 1 && @collection.current_page - 1
+      previous_or_next_page(num, "left")
+    end
+
+    def next_page
+      num = @collection.current_page < @collection.total_pages && @collection.current_page + 1
+      previous_or_next_page(num, "right")
+    end
+
+    def ul_class
+      ["pagination ui menu", @options[:class]].compact.join(" ")
+    end
+  end
+end
